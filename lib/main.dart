96f25174e71c5c0f957b7346dc9bee48bd660358
Gianluca Moratto <gianluca.moratto@beantech.it>
2020-11-05 14:37:53 +0100

    setup firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,23 @@
+import 'package:chat_app_flutter/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Chat',
-      theme: ThemeData(),
-      home: null,
-    );
+    return FutureBuilder(
+        // Initialize FlutterFire:
+        future: _initialization,
+        builder: (context, appSnapshot) {
+          return MaterialApp(
+            title: 'Flutter Chat',
+            theme: ThemeData(),
+            home: ChatScreen(),
+          );
+        });
   }
 }
